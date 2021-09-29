32b497287148571ecb291b954552a732248ad6cc
Aadarsh Mehdi <63882136+amhkhowaja@users.noreply.github.com>
2021-09-29 16:33:03 +0500

    Create Climbing the Leaderboard.cpp
    It is not valid for bigger vectors length like 10000, it times out

---
+++ Climbing the Leaderboard.cpp
@@ -0,0 +1,152 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'climbingLeaderboard' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY ranked
+ *  2. INTEGER_ARRAY player
+ */
+vector<int> ranker(vector<int> vex){
+    vector<int> output;
+    int rank=1;
+    output.push_back(rank);
+    int length=vex.size();
+    for (int i =1;i<length;i++){
+        if (vex[i]<vex[i-1]){
+            rank++;
+        }
+        
+        output.push_back(rank);
+    }
+    return output;
+} 
+int index_of_number(vector<int> vex, int number){
+    int index;
+    for (int i =0; i<vex.size();i++){
+        if (number==vex[i]){
+            index=i;
+            break;
+        }
+    }
+    return index;
+}
+vector<int> climbingLeaderboard(vector<int> ranked, vector<int> player) {
+    vector<int> output;
+    //We have to set the ranking for the ranked vector first ,
+    //then we have to compare it with the current player
+    for (int i=0;i<player.size();i++){
+        int number=player[i];
+        vector<int> cop;
+        copy(ranked.begin(),ranked.end(), back_inserter(cop));
+        cop.push_back(player[i]);
+        sort(cop.begin(),cop.end(), greater<int>());
+        int index=index_of_number(cop,number);
+        vector<int> ranks=ranker(cop);
+        output.push_back(ranks[index]);
+    }
+    return output;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string ranked_count_temp;
+    getline(cin, ranked_count_temp);
+
+    int ranked_count = stoi(ltrim(rtrim(ranked_count_temp)));
+
+    string ranked_temp_temp;
+    getline(cin, ranked_temp_temp);
+
+    vector<string> ranked_temp = split(rtrim(ranked_temp_temp));
+
+    vector<int> ranked(ranked_count);
+
+    for (int i = 0; i < ranked_count; i++) {
+        int ranked_item = stoi(ranked_temp[i]);
+
+        ranked[i] = ranked_item;
+    }
+
+    string player_count_temp;
+    getline(cin, player_count_temp);
+
+    int player_count = stoi(ltrim(rtrim(player_count_temp)));
+
+    string player_temp_temp;
+    getline(cin, player_temp_temp);
+
+    vector<string> player_temp = split(rtrim(player_temp_temp));
+
+    vector<int> player(player_count);
+
+    for (int i = 0; i < player_count; i++) {
+        int player_item = stoi(player_temp[i]);
+
+        player[i] = player_item;
+    }
+
+    vector<int> result = climbingLeaderboard(ranked, player);
+
+    for (size_t i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
